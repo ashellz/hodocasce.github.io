33ed3a7169834545551871b10d4b668ba9ee4857
ashellz <ibrsboy32@gmail.com>
2022-03-12 23:00:52 +0100

    v0.0.0.5
    What's new?
    - Upgraded to a new better website

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function homebutton() {
-    open(url="index.html","_self");
-}
-function aboutbutton() {
-    open(url="about.html","_self");
-}
-function registerbutton() {
-    open(url="register.html","_self");
-}
-function iBakir() {
-    open(url="https://instagram.com/indzicbakir?utm_medium=copy_link")
-}
-function iAmar() {
-    open(url="https://instagram.com/_amar.jsc?utm_medium=copy_link")
-}
-function iHazim() {
-    open(url="https://instagram.com/perenda.h?utm_medium=copy_link")
-}
-function iZijad() {
-    open(url="https://instagram.com/zijadcajdin?utm_medium=copy_link")
+function github() {
+    open(url="https://github.com/ashellz/hodocasce.github.io")
 }
