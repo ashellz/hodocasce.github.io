358b2b43025d791beb6d90ac7036ec0c410f4e14
ashellz <ibrsboy32@gmail.com>
2022-04-18 23:25:01 +0200

    v0.0.1.1
    What's new?
    - Updated font (now works on every device)
    - Added certain borders
    - Changed icons and their hyperlinks
    - Added very stable responsibility for PC's
    - Bug fixes

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 function github() {
     open(url="https://github.com/ashellz/hodocasce.github.io")
 }
+
+function stackoverflow() {
+    open(url="https://stackoverflow.com/users/18823553/shellz")
+}
+
+function discord() {
+    open(url="https://discord.gg/2wgw6hDmxA")
+}
+
+function youtube() {
+    open(url="https://www.youtube.com/channel/UCIJqqAuoRhTi65-K4VGis1Q")
+}
+
+function instagram() {
+    open(url="https://www.instagram.com/ibrahim.becirovic27/")
+}
+
+function snapchat() {
+    open(src="snap.html")
+}
+
+function twitter() {
+    open(url="https://twitter.com/ShellzGota")
+}
+
+function reddit() {
+    open(url="https://www.reddit.com/user/ShellzGota32")
+}
